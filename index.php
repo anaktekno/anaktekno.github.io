1123b95cd7acbd397bdf10bd015bb0e5b571c6c4
anaktekno <87498948+anaktekno@users.noreply.github.com>
2021-07-18 01:20:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="INSPIRO" />
+    <meta name="description" content="Themeforest Template Polo">
+    <!-- Document title -->
+    <title>POLO | The Multi-Purpose HTML5 Template</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,800,700,600|Montserrat:400,500,600,700|Raleway:100,300,600,700,800" rel="stylesheet" type="text/css" />
+    <link href="css/plugins.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet"> </head>
+
+<body>
+
+    <?php
+    include ('header.php');
+    ?>
+
+
+        <!-- Content -->
+        <section id="page-content">
+            <div class="container">
+                <!-- post content -->
+
+                <!-- Page title -->
+                <div class="page-title">
+                    <h1>Anak Tekno</h1>
+
+                </div>
+                <!-- end: Page title -->
+
+                <!-- Blog -->
+                <div id="blog" class="grid-layout post-1-columns m-b-30" data-item="post-item">
+
+                    <!-- Post item-->
+                    <div class="post-item border">
+                        <div class="post-item-wrap">
+                            <div class="post-image">
+                                <a href="#"> <img alt="" src="images/tekno.png"> </a> <span class="post-meta-category"><a href="">Tekno</a></span> </div>
+                            <div class="post-item-description"> <span class="post-meta-date"><i class="fa fa-calendar-o"></i>Jul 14, 2021</span>
+                                <h2><a href="#">Perkembangan Teknologi Dunia </a></h2>
+                                <p>Bila berdialog tentang teknologi , pastinya tidak dapat dipisahkan dari kehidupan manusia . Selamanya , sepanjang peradaban manusia masih terdapat , teknologi hendak terus jadi perihal terutama dalam kehidupan.</p>
+                                <a href="#" class="item-link">Read More <i class="fa fa-arrow-right"></i></a> </div>
+                        </div>
+                    </div>
+                    <!-- end: Post item-->
+
+
+
+                </div>
+                <!-- end: Blog -->
+
+
+
+            </div>
+            <!-- end: post content -->
+
+        </section>
+        <!-- end: Content -->
+        <!-- Footer -->
+      <?php include ('footer.php'); ?>
+        <!-- end: Footer -->
+
+    </div>
+    <!-- end: Wrapper -->
+
+    <!-- Go to top button -->
+    <a id="goToTop"><i class="fa fa-angle-up top-icon"></i><i class="fa fa-angle-up"></i></a>
+    <!--Plugins-->
+     <script src="js/jquery.js"></script>
+     <script src="js/plugins.js"></script>
+
+    <!--Template functions-->
+     <script src="js/functions.js"></script>
+</body>
+
+</html>
