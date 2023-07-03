52f87d78384a42fd432e6b278de86efd4c180434
anaktekno <87498948+anaktekno@users.noreply.github.com>
2023-07-03 18:16:05 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer id="footer" class="footer-light">
-
-    <div class="copyright-content">
-        <div class="container">
-            <div class="copyright-text text-center">&copy; 2021 AnakTekno <a href="#" target="_blank">AnakTekno</a> </div>
-        </div>
-    </div>
-</footer>
-<!-- end: Footer -->
